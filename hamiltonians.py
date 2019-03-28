0ab9d762bf0a1b6ae3a74767207f24788f1d01cb
rbondesan <r.bondesan@gmail.com>
2019-03-28 21:50:28 +0000

    Add Calogero-Moser: trajectory algorithm works nicely

--- hamiltonians.py
+++ hamiltonians.py
@@ -108,13 +108,49 @@ def closed_toda_3(x):
     V = tf.exp(-2.*y) + tf.exp(y - tf.sqrt(3.)*x) + tf.exp(y + tf.sqrt(3.)*x)
     return tf.reduce_sum(tf.square(p), axis=2) + V
 
-# def diff_square_hamiltonian(x, eps=0):
-#     """1/2 \sum_{i=1}^N  [p_i^2 + eps * q_i^2 + (q_{i} - q_{i+1})^2] (with q_{N+1} = q_1)
-#     x.shape = (batch, phase_space, 1)
-#     Here eps is a regulator to make the density e^-H normalizable, avoiding zero mode.
-#     """
-#     assert(x.shape[2] == 1)
-#     q, p = extract_q_p(x)
-#     qdiff = q - tf.manip.roll(q, shift=-1, axis=1) # q - (q_2, q_3, ..., q_{N}, q_1)
-#     return 0.5 * tf.reduce_sum(tf.square(p) + + 0.5 * eps * tf.square(q) + tf.square(qdiff),  axis=1)
-#
+def calogero_moser(x, type, omegasq=1., gsq=1., mu=1.):
+    """
+    Notation: https://www1.maths.leeds.ac.uk/~siru/papers/p38.pdf (2.38)
+    and http://www.scholarpedia.org/article/Calogero-Moser_system
+    
+    H = \frac{1}{2}\sum_{i=1}^n (p_i^2 + \omega^2 q_i^2) + g^2 \sum_{1\le j < k \le n} V(q_j - q_k)
+    
+    V(x) = 
+    'rational':      1/x^2
+    'hyperbolic':    \mu^2/4\sinh(\mu x/2)
+    'trigonometric': \mu^2/4\sin(\mu x/2)
+    """
+    assert(x.shape[1] == 1)
+
+    if type == 'rational':
+        V = lambda x : 1/x**2
+    elif type == 'hyperbolic':
+        V = lambda x : mu**2/4./tf.sinh(mu/2. * x)
+    elif type == 'trigonometric':
+        V = lambda x : mu**2/4./tf.sin(mu/2. * x)
+    else:
+        raise NotImplementedError
+    
+    q, p = extract_q_p(x)
+    h_free = 0.5 * tf.reduce_sum(tf.square(p) + omegasq * tf.square(q), axis=[1,2,3]) # (batch,)
+        
+    # Compute matrix of deltaq q[i]-q[j] and extract upper triangular part (triu)
+    q = tf.squeeze(q, 1) # (N,n,1)
+    deltaq = tf.transpose(q, [0, 2, 1]) - q # (N,n,n)
+    n = tf.shape(deltaq)[1]
+    ones = tf.ones([n,n])
+    triu_mask = tf.cast(tf.matrix_band_part(ones, 0, -1) - \
+                        tf.matrix_band_part(ones, 0, 0), dtype=tf.bool)    
+    triu = tf.boolean_mask(deltaq, triu_mask, axis=1)    
+    eps = 1e-5 # regulizer for inverse
+    h_int = gsq * tf.reduce_sum(V(triu + eps), axis=1) # (batch,)
+    
+    return h_free + h_int
+
+# sum_{i<j} V(x(i) - x(j)) : 
+    # e.g.: 
+    # x = np.arange(10)
+    # x = np.expand_dims(x, 1)
+    # diffs = np.transpose(x) - x
+    # idx = np.triu_indices(np.shape(diffs)[1],k=1) 
+    # np.sum(V(diffs[idx]))
--- tests/thamiltonians.py
+++ tests/thamiltonians.py
@@ -119,36 +119,20 @@ def test_closed_toda_3():
     print('test_closed_toda_3 passed')
 test_closed_toda_3()
 
-# TODO: update
-# def test_oscillator_hamiltonian():
-#     x = tf.reshape(tf.range(1,5,dtype=DTYPE),[1,4,1]) # q = [1,2], p=[3,4]
-#     h = oscillator_hamiltonian(x)
-#     expected_h = tf.constant(1/2 * (9 + 16 + 1 + 4), dtype=DTYPE)
-#     assert_equal(h, expected_h)
-#     print('test_oscillator_hamiltonian passed')
-# test_oscillator_hamiltonian()
-#
-# def test_diff_square_hamiltonian():
-#     x = tf.reshape(tf.range(1,7,dtype=DTYPE),[1,6,1]) # q = [1,3,5], p=[2,4,6]
-#     h = diff_square_hamiltonian(x)
-#     # expected = 1/2 * (4 + 16 + 36 + (1.-3.)**2 + (3.-5.)**2 + (5.-1.)**2)
-#     expected_h = tf.constant(1/2 * (4 + 16 + 36 + 4 + 4 + 16), dtype=DTYPE)
-#     assert_equal(h, expected_h)
-#     print('test_diff_square_hamiltonian passed')
-# test_diff_square_hamiltonian()
-#
-# def test_fpu_alpha_hamiltonian():
-#     x = tf.reshape(tf.range(1,7,dtype=DTYPE),[1,6,1]) # q = [1,3,5], p=[2,4,6]
-#     h = fpu_alpha_hamiltonian(x)
-#     expected_h = tf.constant(1/2 * (4 + 16 + 36 + 4 + 4 + 16) + 1/3 * (- 8 - 8 + 64), dtype=DTYPE)
-#     assert_equal(h, expected_h)
-#     print('test_fpu_alpha_hamiltonian passed')
-# test_fpu_alpha_hamiltonian()
-#
-# def test_fpu_beta_hamiltonian():
-#     x = tf.reshape(tf.range(1,7,dtype=DTYPE),[1,6,1]) # q = [1,3,5], p=[2,4,6]
-#     h = fpu_beta_hamiltonian(x)
-#     expected_h = tf.constant(1/2 * (4 + 16 + 36 + 4 + 4 + 16) + 1/4 * (16 + 16 + 4**4), dtype=DTYPE)
-#     assert_equal(h, expected_h)
-#     print('test_fpu_beta_hamiltonian passed')
-# test_fpu_beta_hamiltonian()
+def test_calogero_moser():
+    x = tf.reshape(tf.range(1,7,dtype=DTYPE),[1,1,3,2]) # q = [1,3,5], p=[2,4,6]
+    h = calogero_moser(x, 'rational')
+    expected_h = tf.constant(
+        1/2 * (4 + 16 + 36 + 1 + 9 + 25) + 1./(1 - 3)**2 + 1./(1 - 5)**2 + 1./(3 - 5)**2,
+        dtype=DTYPE)
+    assert_allclose(h, expected_h)
+
+    x = tf.reshape([-.1,1.,3.2,-.2,1.34,3.],[1,1,3,2]) # q = [-.1,3.2,1.34], p=[1.,-.2,3.]
+    h = calogero_moser(x, 'rational', omegasq=.3)
+    expected_h = tf.constant(
+        1/2 * (1.**2 + .2**2 + 3.**2 + .3 * (.1**2 + 3.2**2 + 1.34**2)) + \
+            1./(-.1 - 3.2)**2 + 1./(-.1 - 1.34)**2 + 1./(3.2 - 1.34)**2,
+        dtype=DTYPE)
+    assert_allclose(h, expected_h)    
+    print('test_calogero_moser passed')
+test_calogero_moser()
